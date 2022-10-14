52c67615ddff3ddb4a0c1e3c6f217c45f23cc7e7
BallinGuy <tballin2000@hotmail.com>
2022-10-14 15:20:18 -0400

    added all of the API fetches and started the dynamic elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,86 @@
 var artistQuery = "coldplay";
 
-function getArtistRecommends(data){
-    fetch("https://floating-headland-95050.herokuapp.com/https://tastedive.com/api/similar?q="+artistQuery+"&k=443399-ClassPro-DVSLXXJW")
+
+
+
+function getArtistRecommends(artist){
+    fetch("https://floating-headland-95050.herokuapp.com/https://tastedive.com/api/similar?q="+artist+"&k=443399-ClassPro-DVSLXXJW&limit=5")
     .then(function (response) {
         console.log("TasteDive response gotten");
         return response.json();
         })
     .then(function (data) {
-        /*if (!data[0]) {
-            return;
-        }*/
         console.log("Data:");
-        console.log(JSON.stringify(data));
+        console.log(JSON.stringify(data.Similar.Results));
+        console.log(data.Similar.Results);
     })
 }
 
-
-function getArtistID(){
-    fetch("https://theaudiodb.com/api/v1/json/2/search.php?s="+artistQuery)
+function getAlbums(artistId){
+    fetch("https://theaudiodb.com/api/v1/json/2/album.php?i="+artistId)
     .then(function (response) {
-        console.log("TADB response gotten");
+        console.log("TADB Album response gotten");
         return response.json();
         })
     .then(function (data) {
-        /*if (!data[0]) {
-            return;
-        }*/
         console.log("Data:");
-        console.log(JSON.stringify(data.artists[0]));
+        var albums = data.album;
+        for (let i = 0; i < 5; i++) {
+            var albumArt = albums[i].strAlbumThumb;
+            var albumName = albums[i].strAlbum;
+            var albumYear = albums[i].intYearReleased;
+            var albumGenre = albums[i].strStyle;
+            var albumLabel = albums[i].strLabel;
+            console.log(albumName);
+            console.log(albumArt);
+            console.log(albumYear);
+            
+            //Create card and append it to body
+            var cardEl = $("<div class='columns'></div>")
+            $("body").append(cardEl); 
+
+            var leftEl = $("<span class='column is-2'></span>")
+            $(cardEl).append(leftEl); 
+            var rightEl = $("<div class='column is-9'></div>")
+            $(cardEl).append(rightEl); 
+            //Create elements and append it to card
+            var albumArtEl = $("<img src='"+albumArt+"' alt='"+albumName+" thumbnail' height='100px' width='100px' >");
+            $(leftEl).append(albumArtEl);
+            var albumNameEl = $("<h4></h4>").text(albumName);
+            var albumYearEl = $("<p></p>").text("Released: "+albumYear);
+            var albumGenreEl = $("<p></p>").text("Released: "+albumGenre);
+            var albumLabelEl = $("<p></p>").text("Record Label: "+albumLabel);
+            $(rightEl).append(albumNameEl, albumYearEl, albumGenreEl, albumLabelEl);  
+            
+        }
+
     })
 }
-getArtistID();
-getArtistRecommends();
+
+function getArtistID(artist){
+    fetch("https://theaudiodb.com/api/v1/json/2/search.php?s="+artist)
+    .then(function (response) {
+        console.log("TADB Artist response gotten");
+        return response.json();
+        })
+    .then(function (data) {
+        console.log("ID: "+JSON.stringify(data.artists[0].idArtist));
+        //parse data
+        var artistArt = data.artists[0].strArtistThumb;
+        var artistName = data.artists[0].strArtist;
+        var artistDescr = data.artists[0].strBiographyEN;
+        /*
+        //Create card and append it to body
+        var cardEl = $("<div'></div>")
+        $("body").append(cardEl); 
+        //Create elements and append it to card
+        var artistArtEl = $("<div><img src='"+artistArt+"' alt='"+artistName+" thumbnail'></div>");
+        var artistNameEl = $("<h4></h4>").text(artistName);
+        var artistDescrEl = $("<p></p>").text(artistDescr);
+        $(cardEl).append(artistArtEl, artistNameEl, artistDescrEl);  
+        */
+        getAlbums(data.artists[0].idArtist);
+    })
+}
+getArtistID(artistQuery);
+//getArtistRecommends(artistQuery);
